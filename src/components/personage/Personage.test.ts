a77b888c8e52a1b2a4943163a77a18c5bc9b4308
Regis Ribeiro <regisgomesr@gmail.com>
2020-11-10 22:52:33 -0300

    Create Personage.test.ts

---
+++ src/components/personage/Personage.test.ts
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow, mount } from "../../../enzyme";
+
+import PersonageComponent from "./index";
+
+describe("List tests", () => {
+  it("should render 10 items", () => {
+    const people = [
+      { name: "Luke Skywalker", eye_color: "blue" },
+      { name: "R2-D2", eye_color: "red" }
+    ];
+    const wrapper = shallow(<PersonageComponent people={people} />);
+
+    console.log(wrapper.debug);
+
+    expect(wrapper.exists()).toBeTruthy();
+    expect(wrapper.find("PersonageComponent")).toHaveLength(people.length);
+  });
+});
